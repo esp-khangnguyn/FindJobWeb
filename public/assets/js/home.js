da9bc23812eddcb7f57527ed9e6ef7ddc402072c
d3m0l15h <davicmax12@gmail.com>
2023-10-02 18:34:06 +0700

    assets

---
+++ public/assets/js/home.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".swiper-jobs", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+console.log($(".swiper-jobs"));
