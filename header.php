3164e113e43e5b1b1bfdec7482e32a726502cd60
Khang <khangdev2813@gmail.com>
2023-10-30 21:32:50 +0700

    update 30/10

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
-    <title>Home - Append Bootstrap Temlate</title>
+    <title>FindJob</title>
     <meta content="" name="description" />
     <meta content="" name="keywords" />
 
@@ -50,7 +50,7 @@
   ======================================================== -->
   </head>
 
-  <body class="index-page" data-bs-spy="scroll" data-bs-target="#navmenu">
+<body class="index-page" data-bs-spy="scroll" data-bs-target="#navmenu">
 <header id="header" class="header fixed-top d-flex align-items-center ">
       <div
         class="container-fluid d-flex align-items-center justify-content-between"
@@ -61,7 +61,7 @@
         >
           <!-- Uncomment the line below if you also wish to use an image logo -->
           <!-- <img src="assets/img/logo.png" alt=""> -->
-          <h1>Find Your Live</h1>
+          <h1>Find Your Job</h1>
           <span>.</span>
         </a>
 
@@ -78,18 +78,18 @@
                 <li><a href="#"></a></li>
                 <li class="dropdown has-dropdown">
                   <a href="#"
-                    ><span>Deep Dropdown</span>
+                    ><span>Theo kĩ năng</span>
                     <i class="bi bi-chevron-down"></i
                   ></a>
                   <ul class="dd-box-shadow">
-                    <li><a href="#">Deep Dropdown 1</a></li>
-                    <li><a href="#">Deep Dropdown 2</a></li>
-                    <li><a href="#">Deep Dropdown 3</a></li>
-                    <li><a href="#">Deep Dropdown 4</a></li>
-                    <li><a href="#">Deep Dropdown 5</a></li>
+                    <li><a href="#">Theo kĩ năng</a></li>
+                    <li><a href="#">Java </a></li>
+                    <li><a href="#">.NET </a></li>
+                    <li><a href="#">Python</a></li>
+                    <li><a href="#">React</a></li>
                   </ul>
                 </li>
-                <li><a href="#">Dropdown 2</a></li>
+                <li><a href="#">Theo kinh nghiệm</a></li>
                 <li><a href="#">Dropdown 3</a></li>
                 <li><a href="#">Dropdown 4</a></li>
               </ul>
@@ -100,7 +100,160 @@
           <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
         <!-- End Nav Menu -->
-
-        <a class="btn-getstarted main-bg" href="index.php#about">Get Started</a>
+        <div class="header-button-group d-flex  flex-row gap-3 ">
+          <button type="button" class="btn-getstarted main-bg border-0 " data-bs-toggle="modal" data-bs-target="#employForm">
+            Nhà tuyển dụng
+          </button>
+          <button type="button" class="btn-getstarted main-bg border-0 " data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Đăng nhập
+          </button>
+        </div>
       </div>
-    </header>
+  </header>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered ">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Đăng nhập</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <label for="recipient-name" class="col-form-label">Tài khoản</label>
+              <input type="text" class="form-control" id="account">
+
+            </div>
+            <div class="mb-3">
+              <label for="message-text" class="col-form-label">Mật Khẩu</label>
+              <input type="password" class="form-control" id="password">
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-primary bg-color border-0 " data-bs-target="#register" data-bs-toggle="modal" >Đăng kí</button>
+          <button type="button" class="btn btn-primary bg-color border-0 " data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" >Quên mật khẩu</button>
+          <button type="button" class="btn btn-primary bg-color border-0">Đăng nhập</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Quên mật khẩu</h1>
+          <button type="button" class="btn-close" data-bs-target="#exampleModal" data-bs-toggle="modal"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <label for="recipient-name" class="col-form-label">Tài khoản</label>
+              <input type="text" class="form-control" id="account">
+            </div>
+            <div class="mb-3">
+              <label for="message-text" class="col-form-label">Mật Khẩu</label>
+              <input type="password" class="form-control" id="password">
+            </div>
+            <div class="mb-3">
+              <label for="message-text" class="col-form-label">Nhập lại mật Khẩu</label>
+              <input type="password" class="form-control" id="confirm-password">
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button class="btn btn-primary" >Xác nhận</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="modal fade" id="register" aria-hidden="true" aria-labelledby="register" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="register">Quên mật khẩu</h1>
+          <button type="button" class="btn-close" data-bs-target="#exampleModal" data-bs-toggle="modal"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <label for="recipient-name" class="col-form-label">Email</label>
+              <input type="text" class="form-control" id="account">
+            </div>
+            <div class="mb-3">
+              <label for="recipient-name" class="col-form-label">Tài khoản</label>
+              <input type="text" class="form-control" id="account">
+            </div>
+            <div class="mb-3">
+              <label for="message-text" class="col-form-label">Mật Khẩu</label>
+              <input type="password" class="form-control" id="password">
+            </div>
+            <div class="mb-3">
+              <label for="message-text" class="col-form-label">Nhập lại mật Khẩu</label>
+              <input type="password" class="form-control" id="confirm-password">
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+              <label class="form-check-label" for="flexCheckDefault">
+                Tôi đã đọc và đồng ý với các Điều khoản dịch vụ và Chính sách quyền riêng tư của FindJob.
+              </label>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button class="btn btn-primary" >Xác nhận</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="modal fade" id="employForm" aria-hidden="true" aria-labelledby="employForm" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="employForm">Quên mật khẩu</h1>
+          <button type="button" class="btn-close"  data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3 d-flex flex-row ">
+              <div class="w-50 p-2 ">
+                <label for="recipient-name" class="col-form-label">Họ tên</label>
+                <input type="text" class="form-control" id="account">
+              </div>
+              <div class="w-50 p-2 ">
+                <label for="message-text" class="col-form-label">Số điện thoại</label>
+                <input type="password" class="form-control" id="password">
+              </div>
+            </div>
+            <div class="mb-3 d-flex flex-row ">
+              <div class="w-50 p-2 ">
+                <label for="recipient-name" class="col-form-label">Emal</label>
+                <input type="text" class="form-control" id="account">
+              </div>
+              <div class="w-50 p-2 ">
+                <label for="message-text" class="col-form-label">Nơi làm việc</label>
+                <input type="password" class="form-control" id="password">
+              </div>
+            </div>
+            <div class="mb-3 d-flex flex-row ">
+              <div class="w-50 p-2 ">
+                <label for="recipient-name" class="col-form-label">Tài khoản</label>
+                <input type="text" class="form-control" id="account">
+              </div>
+              <div class="w-50 p-2 ">
+                <label for="message-text" class="col-form-label">Mật Khẩu</label>
+                <input type="password" class="form-control" id="password">
+              </div>
+            </div>
+
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button class="btn btn-primary" >Xác nhận</button>
+        </div>
+      </div>
+    </div>
+  </div>
--- search.php
+++ search.php
@@ -1,14 +1,64 @@
 
   <?php include 'header.php'  ?>
   <link rel="stylesheet" href="./assets/css/search.css">
-  <main id="main">
 
+
+  <div class="modal fade" id="filterModal" tabindex="-1" aria-labelledby="filterModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered ">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Bộ lọc</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <h3 class="text-black fw-700">Cấp bậc</h3>
+          <input type="checkbox" class="btn-check" id="btn-check" autocomplete="off" >
+          <label class="btn btn-primary bg-transparent text-black border-1 border-color-primary" for="btn-check">Fresher</label>
+          <input type="checkbox" class="btn-check" id="btn-check-2" autocomplete="off" >
+          <label class="btn btn-primary bg-transparent text-black border-1 border-color-primary" for="btn-check-2">Junior</label>
+          <input type="checkbox" class="btn-check" id="btn-check-3" autocomplete="off" >
+          <label class="btn btn-primary bg-transparent text-black border-1 border-color-primary" for="btn-check-3">Senior</label>
+          <input type="checkbox" class="btn-check" id="btn-check-4" autocomplete="off" >
+          <label class="btn btn-primary bg-transparent text-black border-1 border-color-primary" for="btn-check-4">Manager</label>
+        </div>
+        <div class="modal-body">
+          <h3 class="text-black fw-700">Loại công ty</h3>
+          <input type="checkbox" class="btn-check" id="btn-check-product" autocomplete="off" >
+          <label class="btn btn-primary bg-transparent text-black border-1 border-color-primary" for="btn-check-product">Fresher</label>
+          <input type="checkbox" class="btn-check" id="btn-check-outsrc" autocomplete="off" >
+          <label class="btn btn-primary bg-transparent text-black border-1 border-color-primary " for="btn-check-outsrc">Junior</label>
+        </div>
+        <div class="modal-body">
+          <h3 class="text-black fw-700">Loại công ty</h3>
+          <input type="checkbox" class="btn-check" id="btn-check-remote" autocomplete="off" >
+          <label class="btn btn-primary bg-transparent text-black border-1 border-color-primary" for="btn-check-remote">Từ xa</label>
+          <input type="checkbox" class="btn-check" id="btn-check-office" autocomplete="off" >
+          <label class="btn btn-primary bg-transparent text-black border-1 border-color-primary " for="btn-check-office">Tại văn phòng</label>
+          <input type="checkbox" class="btn-check" id="btn-check-flex" autocomplete="off" >
+          <label class="btn btn-primary bg-transparent text-black border-1 border-color-primary " for="btn-check-flex">Linh hoạt</label>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary bg-color border-0 "  data-bs-dismiss="modal" >Áp dụng</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <main id="main">
     <!-- Blog Page Title & Breadcrumbs -->
     <div data-aos="fade" class="page-title">
       <div class="heading">
         <div class="container">
           <div class="row d-flex justify-content-center text-center">
             <div class="col-lg-8 w-100 d-flex justify-content-center gap-2   flex-row">
+              <select class="form-select w-auto" aria-label="Default select example">
+                <option selected>Địa điểm</option>
+                <option value="1">Hồ Chí Minh</option>
+                <option value="2">Hà Nội</option>
+                <option value="3">Đà nẵng </option>
+                <option value=""> Cần Thơ</option>
+                <option value="">Huế</option>
+              </select>
               <input class="w-50 p-3 rounded-2 " type="text" placeholder="Nhập từ khóa" >
               <button class="w-auto p-3 rounded-2 bg-color d-flex justify-content-center align-content-center border-0 ">
                 <img src="./assets/img/search.png" width="32px" height="32px;" alt="">
@@ -18,6 +68,7 @@
           </div>
           <div class="row mt-2 ">
             <ul class="row d-flex  flex-row bull list-unstyled justify-content-center ">
+              <li class="w-auto p-2  ">Gợi ý: </li>
               <li class="w-auto p-2 rounded-2 border-light border-1 ">Java</li>
               <li class="w-auto p-2 rounded-2 border-light border-1 ">React</li>
               <li class="w-auto p-2 rounded-2 border-light border-1 ">.NET</li>
@@ -34,7 +85,9 @@
     <section id="blog" class="blog">
 
       <div class="container" data-aos="fade-up" data-aos-delay="100">
-
+        <button class="w-auto p-3 rounded-2 bg-color d-flex justify-content-center align-content-center border-0 mb-2"  data-bs-toggle="modal" data-bs-target="#filterModal">
+         <p class="m-0 fw-bold text-light "> Lọc</p>
+        </button>
         <div class="row gy-4 posts-list">
 
           <div class="col-xl-6 col-lg-6">
